b6ff9815172a875842e4d7f15ff89d214e971ce7
hshakeshaft <henry.shakeshaft:@live.co.uk>
2025-06-16 20:37:41 +0100

    basic var statement parsing

--- code/parser.c
+++ code/parser.c
@@ -1,7 +1,52 @@
+#include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "parser.h"
 #include "lexer.h"
+#include "util.h"
+
+const char *tyger_error_kind_to_string(Tyger_Error_Kind kind)
+{
+  const char *str;
+
+  switch (kind)
+  {
+#define X(NAME) case TYERR_##NAME: { str = #NAME; } break;
+    #include "defs/tyger-error-kind.def"
+#undef X
+
+  default:
+  {
+    fprintf(stderr, "[ERROR] Invalid error kind encountered: %i\n", kind);
+    str = NULL;
+    assert(0);
+  } break;
+  }
+
+  return str;
+}
+
+const char *statement_kind_to_string(Statement_Kind kind)
+{
+  const char *str;
+
+  switch (kind)
+  {
+#define X(NAME) case STMT_##NAME: { str = #NAME; } break;
+    #include "defs/statement-kind.def"
+#undef X
+
+  default:
+  {
+    fprintf(stderr, "[ERROR] Invalid statement kind encountered: %i\n", kind);
+    str = NULL;
+    assert(0);
+  } break;
+  }
+
+  return str;
+}
 
 static void parser_next_token(Parser *p)
 {
@@ -18,21 +63,93 @@ void parser_init(Parser *p, Lexer *lx)
   
 Program parser_parse_program(Parser *p)
 {
-  (void*) p;
   Program program = {0};
+  va_array_init(Tyger_Error, program.errors);
+  va_array_init(char, program.statements);
+
   while (p->cur_token.kind != TK_EOF)
   {
+    Statement stmt = {0};
+    Tyger_Error err = parser_parse_statement(p, &stmt);
+
+    if (err.kind == TYERR_NONE)
+    {
+      va_array_append(program.errors, err);
+    }
+    else
+    {
+      va_array_append(program.statements, stmt);
+    }
     parser_next_token(p);
   }
+
   return program;
 }
 
-const char *program_to_string(const Program *p)
+Tyger_Error parser_parse_statement(Parser *p, Statement *stmt)
 {
-  (void*) p;
-  #define PTS_TEST_STR "Hello, World!\n"
-  char *buffer = malloc(sizeof(char) * sizeof(PTS_TEST_STR) + 1);
-  snprintf(buffer, sizeof(PTS_TEST_STR) + 1, PTS_TEST_STR);
-  buffer[sizeof(PTS_TEST_STR)] = '\0';
-  return buffer;
+  Tyger_Error err = {0};
+
+  switch (p->cur_token.kind)
+  {
+  case TK_VAR:
+  {
+    err = parse_var_statement(p, stmt);
+  } break;
+
+  default:
+  {
+    fprintf(
+      stderr, "[ERROR] unexpected value for Token_Kind: %i\n", p->cur_token.kind
+    );
+    assert(0);
+  } break;
+  }
+
+  return err;
+}
+
+static inline bool peek_token_is(Parser *p, Token_Kind kind)
+{
+  return p->cur_token.kind == kind;
+}
+
+static inline bool expect_peek(Parser *p, Token_Kind kind)
+{
+  if (peek_token_is(p, kind))
+  {
+    parser_next_token(p);
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+Tyger_Error parse_var_statement(Parser *p, Statement *stmt)
+{
+  Tyger_Error err = {0};
+
+  if (!expect_peek(p, TK_IDENT))
+  {
+    err.kind = TYERR_SYNTAX;
+  }
+
+  // TODO(HS): read ident
+
+  if (!expect_peek(p, TK_ASSIGN))
+  {
+    err.kind = TYERR_SYNTAX;
+  }
+
+  // TODO(HS): parse expression
+  while (p->cur_token.kind != TK_SEMICOLON)
+  {
+    parser_next_token(p);
+  }
+
+  stmt->kind = STMT_VAR;
+
+  return err;
 }
--- includes/parser.h
+++ includes/parser.h
@@ -3,16 +3,40 @@
 #include <stddef.h>
 #include "lexer.h"
 
+typedef enum tyger_error_kind
+{
+#define X(NAME) TYERR_##NAME,
+  #include "defs/tyger-error-kind.def"
+#undef X
+} Tyger_Error_Kind;
+
+typedef enum statement_kind
+{
+#define X(NAME) STMT_##NAME,
+  #include "defs/statement-kind.def"
+#undef X
+} Statement_Kind;
+
+typedef struct tyger_error
+{
+  Tyger_Error_Kind kind;
+} Tyger_Error;
+
+typedef struct statement
+{
+  Statement_Kind kind;
+} Statement;
+
 typedef struct statement_vaarray
 {
-  char *ptr;
+  Statement *elems;
   size_t capacity;
   size_t len;
 } Statement_VaArray;
 
 typedef struct error_vaarray
 {
-  char *ptr;
+  Tyger_Error *elems;
   size_t capacity;
   size_t len;
 } Error_VaArray;
@@ -32,6 +56,11 @@ typedef struct program
 
 void parser_init(Parser *p, Lexer *lx);
 Program parser_parse_program(Parser *p);
-const char *program_to_string(const Program *p);
+
+const char *tyger_error_kind_to_string(Tyger_Error_Kind kind);
+const char *statement_kind_to_string(Statement_Kind kind);
+
+Tyger_Error parser_parse_statement(Parser *p, Statement *stmt);
+Tyger_Error parse_var_statement(Parser *p, Statement *stmt);
 
 #endif // TYGER_PARSER_H_
