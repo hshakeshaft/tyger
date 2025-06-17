cccb7916dfd1f3d39d6d3a6d237c20d169cffbc7
hshakeshaft <henry.shakeshaft:@live.co.uk>
2025-06-18 00:15:12 +0100

    add String_Builder struct

--- includes/tstrings.h
+++ includes/tstrings.h
@@ -9,6 +9,13 @@ typedef struct string_view
   size_t len;
 } String_View;
 
+typedef struct string_builder
+{
+  char *buffer;
+  size_t capacity;
+  size_t len;
+} String_Builder;
+
 #if defined(__cplusplus)
 #define make_string_view(STR, LEN) String_View{ (char*) (STR), (LEN) }
 #else
@@ -23,5 +30,9 @@ typedef struct string_view
 bool string_view_eq(String_View sv1, String_View sv2);
 bool string_view_eq_str(String_View sv, const char *str);
 void string_view_format_buffer(char *buffer, size_t buffer_len, String_View sv);
+
+void string_builder_init(String_Builder *sb);
+void string_builder_append_fmt(String_Builder *sb, const char *fmt, ...);
+const char *string_builder_to_cstring(const String_Builder *sb);
 
 #endif // TYGER_TSTRINGS_H_
