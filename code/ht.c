e591e1c86f662001a8e5dd61e4dd4ba9d4c3ac73
hshakeshaft <henry.shakeshaft@live.co.uk>
2026-09-11 22:54:06 +0100

    refactor hash table insert internals

--- code/ht.c
+++ code/ht.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -45,25 +46,23 @@ void ht_deinit(HT *ht)
 }
 
 
-/* TODO(HS): hash collision resolution */
-
 void ht_insert(HT *ht, const char *key, TyObject *value)
 {
     HT_Header *header;
     unsigned int hash;
     size_t key_len;
     size_t bucket;
-    HT_Slot slot_info;
+    HT_Slot *slot_info;
 
     header  = ((HT_Header*) *ht) - 1;
     key_len = strlen(key);
     hash    = ht__djb2_hash_key(key, key_len);
     bucket  = hash % header->buckets;
 
-    slot_info.key   = key;
-    slot_info.value = value;
-    slot_info.next  = NULL;
-    memcpy(&(*ht)[bucket], &slot_info, sizeof(slot_info));
+    slot_info = &(*ht)[bucket];
+    slot_info->key = key;
+    slot_info->value = value;
+    slot_info->next = NULL;
 }
 
 HT_Slot *ht_get(HT *ht, const char *key)
--- tests/test_hash_table.cpp
+++ tests/test_hash_table.cpp
@@ -6,11 +6,86 @@
 /// and as such the VM wraps all hash table functions; however the hash table is
 /// integrally linked with the concept of a `TyObject` and thus cannot be divoreced.
 ///
+#include <gtest/gtest.h>
+#include <string>
+#include "helpers/helpers.hpp"
+#include "helpers/tyger.hpp"
+
+
+/// Helper type which manages the lifetime of a TyObject within the hash tablke
+struct HTTestValue
+{
+    HTTestValue(std::string&& t_ident, int t_value)
+        : ident{t_ident}, value{t_value}
+    {
+        this->object = tyobject_create(OBJ_INTEGER, &t_value);
+    }
+
+    ~HTTestValue()
+    {
+        tyobject_destroy(this->object);
+    }
+
+    std::string ident;
+    TyObject *object;
+    int value;
+};
+
 
 // TODO: test insertion
-//  - [ ] insert lone values
-//  - [ ] insert multiple values which don't collide
+//  - [x] insert lone values
+//  - [x] insert multiple values which don't collide
 //  - [ ] insert when hash collision occurs ("separate chaining")
+TEST(HashTableTestSuite, user_can_insert_into_hash_table)
+{
+    HT ht;
+    ht_init(&ht, 1);
+
+    auto val = HTTestValue("foo", 100);
+    ht_insert(&ht, val.ident.c_str(), val.object);
+
+    HT_Slot slot = ht[0];
+    ASSERT_EQ(std::string(slot.key), val.ident);
+    ASSERT_EQ(slot.value->as.integer, val.value);
+
+    ht_deinit(&ht);
+}
+
+
+/* NOTE(HS): precomputed slot indexes using the following python program
+```
+def djb2(key: str) -> int:
+  hash = 5381
+  for c in list(key):
+    hash = ((hash << 5) + hash) + ord(c)
+  return hash
+print(djb2("foo") % 8)  # 1
+print(djb2("bar") % 8)  # 2
+```
+*/
+TEST(HashTableTestSuite, use_can_insert_multiple_non_colliding_values)
+{
+    HT ht;
+    ht_init(&ht, 8);
+
+    auto val1 = HTTestValue("foo", 100);
+    auto val2 = HTTestValue("bar", 50);
+
+    ht_insert(&ht, val1.ident.c_str(), val1.object);
+    ht_insert(&ht, val2.ident.c_str(), val2.object);
+
+    HT_Slot slot1 = ht[1];
+    HT_Slot slot2 = ht[2];
+
+    ASSERT_EQ(std::string(slot1.key), val1.ident);
+    ASSERT_EQ(std::string(slot2.key), val2.ident);
+
+    ASSERT_EQ(slot1.value->as.integer, val1.value);
+    ASSERT_EQ(slot2.value->as.integer, val2.value);
+
+    ht_deinit(&ht);
+}
+
 
 // TODO: test retrieval
 //  - [ ] test retrieval when no values in table (===> return NULL)
